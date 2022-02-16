29f93e78823fb193162957e8c6f235e876162946
Qxtlp <xi.tulop@yandex.com>
2022-02-16 21:06:14 +0700

    funtional game

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const choice = ["Rock","Paper","Scissors"];
 
 function computerPlay(){
     let random = Math.floor(Math.random() *3);
-    return choice[random];
+    return random;
 }
 
 function playerSelection(){
@@ -11,12 +11,28 @@ function playerSelection(){
     if (adjusted.includes(input.toLocaleLowerCase())) {
         return adjusted.indexOf(input.toLocaleLowerCase());
     }else{
-        console.log("There's no such option on rock-paper-scissor game");
+        console.log("Please pick either rock paper or scissor");
     }
 }
+function playRound(playerSelection,computerPlay){
+    let cp = computerPlay;
+    let p1 = playerSelection;
+    
+    if (p1==0&&cp==0||p1==1&&cp==1||p1==2&&cp==2) {
+        return "draw";
+    }else if (p1==0&&cp==1||p1==1&&cp==2||p1==2&&cp==0) {
+        return "lose";
+    }else{
+        return "win";
+    }
+    
+}
 
-let cp = computerPlay();
-let player = playerSelection();
-console.log(cp);
-console.log(player);
-playerSelection();
+const player = playerSelection();
+const computer = computerPlay();
+
+console.log(
+    "you chose: "+choice[player],
+    "\ncomputer chose: "+choice[computer],
+    "\nresult: "+playRound(player,computer)
+)
