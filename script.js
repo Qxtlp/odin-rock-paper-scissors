1b555d5862b363b463c185e826bcf0991c8d9a35
Qxtlp <xi.tulop@yandex.com>
2022-02-16 20:38:01 +0700

    add player function

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+const choice = ["Rock","Paper","Scissors"];
+
 function computerPlay(){
-    const choice = ["Rock","Paper","Scissors"];
     let random = Math.floor(Math.random() *3);
     return choice[random];
 }
 
+function playerSelection(){
+    const input = prompt("What's your choise?");
+    const adjusted = choice.map(value => value.toLowerCase());
+    if (adjusted.includes(input.toLocaleLowerCase())) {
+        return adjusted.indexOf(input.toLocaleLowerCase());
+    }else{
+        console.log("There's no such option on rock-paper-scissor game");
+    }
+}
+
 let cp = computerPlay();
+let player = playerSelection();
 console.log(cp);
+console.log(player);
+playerSelection();
